fbbbd907cbd2ced78e33b3bbe7fa0162cef4127a
NkBe <nikobe918@outlook.com>
2026-07-11 19:26:24 +0800

    actions: 優化 CI 流程並更新建置環境

--- app/src/main/java/io/github/bbzq/BbzqApplication.kt
+++ app/src/main/java/io/github/bbzq/BbzqApplication.kt
@@ -5,6 +5,10 @@ import android.app.Application
 class BbzqApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (LinkerGuard.hasConflict(this)) {
+            LinkerGuard.triggerConflict()
+            return
+        }
         ModuleRemotePreferences.init(this)
         applyDesktopIconSetting()
     }
